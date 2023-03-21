41b28aa7944269b077e3d1e029613b1bb3310157
CasperDeduct <ruslan.masuk@gmail.com>
2023-03-22 00:08:36 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# addon-serial
+## Serial bundle
+
+Serial bundle includes:
+
+1. Serial console plugin for usb/com port usage
+2. Need fulfill!
---
+++ src/main/java/org/touchhome/bundle/serial/RawSerialPortCommunicator.java
@@ -0,0 +1,52 @@
+package org.touchhome.bundle.serial;
+
+import static com.fazecast.jSerialComm.SerialPort.TIMEOUT_NONBLOCKING;
+
+import com.fazecast.jSerialComm.SerialPort;
+import java.util.Collection;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+import org.touchhome.bundle.api.EntityContext;
+import org.touchhome.bundle.api.console.ConsolePluginCommunicator;
+import org.touchhome.bundle.api.console.ConsolePluginComplexLines;
+import org.touchhome.bundle.api.port.BaseSerialPort;
+import org.touchhome.bundle.api.port.PortFlowControl;
+
+@Log4j2
+public class RawSerialPortCommunicator extends BaseSerialPort {
+
+  private final ConsolePluginCommunicator communicatorConsolePlugin;
+  @Getter
+  private final CircularFifoQueue<ConsolePluginComplexLines.ComplexString> buffer = new CircularFifoQueue<>(1000);
+
+  public RawSerialPortCommunicator(SerialPort serialPort, EntityContext entityContext,
+      ConsolePluginCommunicator consolePluginCommunicator) {
+    super("", "Serial", entityContext, 9600, PortFlowControl.FLOWCONTROL_OUT_NONE, () ->
+        entityContext.ui().sendErrorMessage("SERIAL_PORT.EXCEPTION"), null, log);
+    this.serialPort = serialPort;
+    this.communicatorConsolePlugin = consolePluginCommunicator;
+  }
+
+  @Override
+  public boolean open(int baudRate, PortFlowControl flowControl) {
+    serialPort.setComPortTimeouts(TIMEOUT_NONBLOCKING, 100, 0);
+    serialPort.setComPortParameters(baudRate, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+
+    return super.open(baudRate, flowControl);
+  }
+
+  @Override
+  @SneakyThrows
+  protected void handleSerialEvent(byte[] buf) {
+    ConsolePluginComplexLines.ComplexString data =
+        ConsolePluginComplexLines.ComplexString.of(new String(buf), System.currentTimeMillis());
+    buffer.add(data);
+    communicatorConsolePlugin.dataReceived(data);
+  }
+
+  public Collection<ConsolePluginComplexLines.ComplexString> getValues() {
+    return buffer;
+  }
+}
